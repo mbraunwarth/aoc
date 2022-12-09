995c947dad77f57c818407c4a0c8b18210eb5057
Markus Braunwarth <17438264+mbraunwarth@users.noreply.github.com>
2022-12-09 22:15:30 +0100

    solved day 3 part 1

--- 2022/03/Main.hs
+++ 2022/03/Main.hs
@@ -1,3 +1,4 @@
+import Data.Char (ord)
 import System.Environment
 
 main :: IO ()
@@ -8,14 +9,20 @@ main = do
     putStrLn (show solution)
 
 solve :: String -> Int
-solve rs = (foldr (priority . share . compartments) 0) $ lines rs
+solve rs = sum $ map (priority . share . compartments) (lines rs)
     where
         -- split one rucksack into its compartments
         compartments :: String -> (String, String)
         compartments r = (take (length r `div` 2) r, drop (length r `div` 2) r)
-
+        
+        -- check compartments for shared items
         share :: (String, String) -> Char
-        share _ = 'a'
-
+        share (ls, rs) = if (head ls) `elem` rs
+                            then head ls
+                            else share ((tail ls), rs)
+        
+        -- get priority of an item
         priority :: Char -> Int
-        priority _ = 1
+        priority c = 
+          let ps = zip (['a'..'z'] ++ ['A'..'Z']) [1..52]
+           in snd $ head $ filter ((==c) . fst) ps
