357c1d6c2807cf0d7c6d958fa744d18dfce769b7
Markus Braunwarth <17438264+mbraunwarth@users.noreply.github.com>
2023-12-02 14:55:16 +0100

    [WIP] day one part two

--- 2023/01/Main.hs
+++ 2023/01/Main.hs
@@ -19,13 +19,34 @@ spelled = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine
 digits = [1 .. 9]
 digitLookup = zip spelled digits
 
+pLine :: String -> String
+pLine line = pLine' line acc
+  where
+    pLine' = 
+      case line of
+        d:rest | isDigit d -> pLine' rest (acc ++ [d])
+        _ -> if pref `isPrefixOf` line 
+               then pLine' (fromJust (stripPrefix pref line)) (acc ++ pWord pref)
+               else acc
+
+pWord :: String -> String
+pWord spelled =
+  case spelled of 
+    "one" -> "1"
+    "two" -> "2"
+    "three" -> "3"
+    "four" -> "4"
+    "five" -> "5"
+    "six" -> "6"
+    "seven" -> "7"
+    "eight" -> "8"
+    "nine" -> "9"
+    _ -> spelled
+
 applyLookup :: String -> String -> String
 applyLookup line acc = 
   case line of
     [] -> acc
-    _ -> if pref `isPrefixOf` line 
-           then applyLookup (fromJust (stripPrefix pref line)) (acc ++ pref)
-           else acc
   where
     pref = "one"
 
